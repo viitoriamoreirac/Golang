fb97b746535310bb0b14a7759cf019a6e1d1dbb2
viitoriamoreirac <viitoriamoreirac@gmail.com>
2023-08-26 10:18:15 -0300

    11 e 12

--- 11_Estruturas_de_controle/estruturas.go
+++ 11_Estruturas_de_controle/estruturas.go
@@ -4,4 +4,18 @@ import "fmt"
 
 func main() {
 	fmt.Println("Estruturas")
+
+	numero := 10
+
+	if numero > 15 {
+		fmt.Println("Maior")
+	} else {
+		fmt.Println("Menor")
+	}
+
+	if outronumero := numero; outronumero > 0 {
+		fmt.Println("Maior,0")
+	} else {
+		fmt.Println("Menor,0")
+	}
 }
---
+++ 12_Switch/switch.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func diasDaSemana(numero int) string {
+	switch numero {
+	case 1:
+		return "Domingo"
+	case 2:
+		return "Segunda"
+	case 3:
+		return "Terça"
+	case 4:
+		return "Quarta"
+	case 5:
+		return "Quinta"
+	case 6:
+		return "Sexta"
+	case 7:
+		return "Sábado"
+	default:
+		return "Inválido"
+	}
+}
+
+func diasDaSemana2(numero int) string {
+	switch{
+	case numero == 1:
+		return "Domingo"
+	case numero == 2:
+		return "Segunda"
+	case numero == 3:
+		return "Terça"
+	case numero == 4:
+		return "Quarta"
+	case numero == 5:
+		return "Quinta"
+	case numero == 6:
+		return "Sexta"
+	case numero == 7:
+		return "Sábado"
+	default:
+		return "Inválido"
+	}
+	}
+}
+
+func main() {
+	fmt.Println(diasDaSemana(3))
+}
